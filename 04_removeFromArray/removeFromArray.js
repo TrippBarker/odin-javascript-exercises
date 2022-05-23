05234836330ae937ac9a8f4d28e93d06ee5ce55b
Tripp Barker <colemanbarker@gmail.com>
2022-05-23 14:11:06 -0400

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...args) {
+    for (let arg of args){
+        for (let i = 0; i < myArray.length; i++){
+            if(myArray[i] === arg){
+                myArray.splice(i, 1);
+                break;
+            }
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
