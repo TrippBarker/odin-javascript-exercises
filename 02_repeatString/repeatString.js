93bd08dbce3c7f39ae85fad4e21e469b392183e5
Tripp Barker <colemanbarker@gmail.com>
2022-05-23 13:39:12 -0400

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let retStr = '';
+    if (num < 0){
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++){
+        retStr += str;
+    }
+    return retStr;
 };
 
 // Do not edit below this line
