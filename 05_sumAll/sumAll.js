c8a89ef62df4758db5df0ba31717ba9931568f6a
Tripp Barker <colemanbarker@gmail.com>
2022-05-23 14:25:10 -0400

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isFinite(num1) || !Number.isFinite(num2)){
+        return 'ERROR';
+    }
+    let nums = [];
+    let total = 0;
+    (num1 - num2 < 0) ? nums = [num1, num2] : nums = [num2, num1];
+    for (let num = nums[0]; num <= nums[1]; num++){
+        total += num;
+    }
+    return total;
 };
 
 // Do not edit below this line
